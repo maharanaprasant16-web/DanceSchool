b8229bac649668f483320a9e01c52853dcd788cb
maharanaprasant16-web <maharanaprasant16@gmail.com>
2026-04-21 23:17:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ hide_streamlit_style = """
             header {visibility: hidden;}
             footer {visibility: hidden;}
             .stAppDeployButton {display: none;}
+            div[data-testid="stFooter"] {display: none;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
